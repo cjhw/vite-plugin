52651f744867dc695fb660d12fba35cc2f545828
caijianhaoa <1784581164@qq.com>
2022-09-10 11:24:03 +0800

    实现svg组件形式加载

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import virtual from './plugins/virtual-module'
+import svgr from './plugins/svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), virtual()],
+  plugins: [react(), virtual(), svgr({ defaultExport: 'component' })],
 })
